e0d1747c29df58631df08b96a95083e0669c530b
Matthew Karas <matthew.james.karas@gmail.com>
2014-08-16 21:14:13 -0400

    Do nothing test setup for bowling game

---
+++ BowlingGame.cpp
@@ -0,0 +1,18 @@
+/*
+ * BowlingGame.cpp
+ *
+ *  Created on: Aug 16, 2014
+ *      Author: matt
+ */
+
+#include "BowlingGame.h"
+
+BowlingGame::BowlingGame() {
+  // TODO Auto-generated constructor stub
+
+}
+
+BowlingGame::~BowlingGame() {
+  // TODO Auto-generated destructor stub
+}
+
---
+++ BowlingGame.h
@@ -0,0 +1,17 @@
+/*
+ * BowlingGame.h
+ *
+ *  Created on: Aug 16, 2014
+ *      Author: matt
+ */
+
+#ifndef BOWLINGGAME_H_
+#define BOWLINGGAME_H_
+
+class BowlingGame {
+ public:
+  BowlingGame();
+  virtual ~BowlingGame();
+};
+
+#endif /* BOWLINGGAME_H_ */
